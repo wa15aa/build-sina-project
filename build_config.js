caa5fe56187cb251d8bd135b389ff7526336a07f
System Administrator <root@wutongtongdeMacBook-Pro.local>
2017-06-09 14:52:23 +0800

    修改传餐模式为传入配置文件模式

---
+++ build_config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by tongtong3 on 2017/6/9.
+ */
+module.exports = {
+    projectPath : '../../projectName',
+    svnPath:'https://svn1.intra.sina.com.cn/sinanews/trunk/ria/items/2017',
+    devHost:'http://test.sina.com.cn',
+    onLinePath:'http://simg.sinajs.cn/products/news/items/2016/international/'
+}
