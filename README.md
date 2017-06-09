4bd006628e51045f6711818166ebc4cae8194c39
wa15aa <772751110@qq.com>
2017-06-09 14:56:40 +0800

    修改用户信息

--- README.md
+++ README.md
@@ -1,21 +1,60 @@
-# build-sina-project
-初始化新浪的项目
+# 安装
+### 安装nodejs LTS版本
+[地址](http://nodejs.org/)
 
-### 环境需要 ###
-SVN命令行
-
-### DEMO ###
- ```
- $ node index.js --dir '/d/yourProjectName' --svn 'https://yourProjectAdds/2017/' --devPubilcPath 'http://test.sina.com.cn' --onLinePublicPath 'http://test.sina.com.cn'
- ```
-### 参数说明 ###
-* dir: 本地目录，最后一个文件夹，作为项目的名字。可以是绝对路径也可以是相对路径，**必填**。
-* svn：svn地址（**非必填**），要求是年份作为url的截止，eg:
-
+### 安装cnpm
 ```
-$ node index.js --dir '/d/workspace/demo2017' --svn 'https://svn.sina.com.cn/news/item/2017/'
-//这样会在trunk下的2017目录下生成demo2017目录，以及在对应的tag目录下生成demo2017目录与qb目录。
+$ npm install cnpm -g --registry=https://registry.npm.taobao.org
 ```
+### 安装依赖模块
+```
+$ cnpm install
+$ cnpm install cross-env -g
+$ cnpm install http-server -g
+$ cnpm install webpack-dev-server -g
+$ cnpm install webpack@1.13.0 -g
+```
+# 运行命令
+### 开发环境（非IE）
+```
+$ npm run dev-build
+```
+### 开发环境（测试ie[低版本]）
+```
+$ npm run dev-ie-build
+$ npm run dev-ie-server
+```
+### 打包上线
+```
+$ npm run build
+```
+# 配置项说明
+```javascript
+    //在根目录下的config.js是，简单的配置文件
+    {
+        publicPath: 'http://test.sina.com.cn', //已http或者https开头的绝对地址
+        onLinePublicPath: 'http://simg.sinajs.cn/products/news/items/2016/', //线上静态资源地址
+        md5: false, //hash 可选值 false：不加md5值 hash：全部资源公用一个hash chunkhash：单文件一个hash值
+        //雪碧图的配资
+        sprites: [{
+            //生成雪碧图的文件名字，该文件夹在pages下生成，eg: pages/sprite/normal,
+            //有必要的话可以生成多个雪碧图的文件夹,后续生成scss的雪碧图映射文件与name
+            //是一致的，同时也会在 img文件夹下面生成对应名字的png文件
+            name: 'normal'
+        }]
+    }
+    //注意雪碧图目前配置仅仅支持pc端的，后续会添加移动端的支持
+```
+# 项目开发目录简单说明
+1. pages/html目录下是开发页面的html构建，不需要引入业务文件
+2. pages/js/page目录下是开发js的主入口文件，注意这个js文件夹下的js文件需要与上面1中的html目录中的html同名，否则无法给对应的html注入对应的js文件
+3. pages/sprite/** 文件夹中的图片是都是要生成雪碧图的
 
-* devPubilcPath：开发环境访问的地址，注意要绑定自己的host(**非必填**)。默认值：http://test.sina.com.cn
-* onLinePublicPath：上线资源文件的地址(**非必填**)。默认值：http://test.sina.com.cn
+
+# change log
+* tagv0.6 修复package.json文件中的运行命令行的一个错误。
+* tagv0.5 因为webpack-dashboard对于git bash模拟器支持不够友好，因此放弃对他的应用。将tinypng的key值转移到config.js进行管理。修复config.js中md5参数不为false的时候，测试环境运行命令会产生错误的问题。
+* tagv0.4 修改命令英文拼写错误
+* tagv0.3 增加README.MD的简要说明
+* tagv0.2 增加PC雪碧图的支持，增加简单的配置支持。
+* tagv0.1 优化原有开发环境代码，采用内存的方式开发代码，添加跨浏览器环境运行命令。
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+#! /usr/bin/env node
+
+
 /**
  * Created by sina on 2016/6/27.
  */
@@ -7,56 +10,59 @@ const svnHandler = require('./lib/svnInit');
 const _ = require('lodash');
 const dirHandler = require('./lib/initDir');
 const fileHandler = require('./lib/copyFile.js');
-
-let argv = require('optimist').default({
-    'dir': process.cwd(),
-    'svn': '',
-    'devPubilcPath': 'http://test.sina.com.cn/',
-    'onLinePublicPath': 'http://test.sina.com.cn/'
-}).argv;
+const devConfig = require('./build_config');
+// console.log(123)
+// debugger;
+// let argv = require('optimist').default({
+//     'dir': process.cwd(),
+//     'svn': '',
+//     'devPubilcPath': 'http://test.sina.com.cn/',
+//     'onLinePublicPath': 'http://test.sina.com.cn/'
+// }).argv;
 
 //TODO 支持选择配置文件的方式
 //TODO 修改readme以及打tag
-
-const projectName = _.last(argv.dir.split(path.sep));
+console.log(devConfig.svnPath);
+const projectName = _.last(devConfig.projectPath.split(path.sep));
 
 //为了防止 用户的system time 错误，因此采用用户自己输入时间
 let svnYear;
 
-if (argv.svn) {
-    let tmpSvnHref = argv.svn.split(path.sep).join('/');
+if (devConfig.svnPath) {
+    let tmpSvnHref = devConfig.svnPath.split(path.sep).join('/');
     //主要是处理 有些内容 返回的结构是 https:\xxx\asdfas\asdfa\ 这种
-    argv.svn = tmpSvnHref.replace(/^(https:\/)([^\/])(.*)/, '$1/$2$3');
+    devConfig.svnPath = tmpSvnHref.replace(/^(https:\/)([^\/])(.*)/, '$1/$2$3');
     try {
-        svnYear = argv.svn.match(/\/(\d+)\/{0,1}$/)[1];
+        svnYear = devConfig.svnPath.match(/\/(\d+)\/{0,1}$/)[1];
     } catch (e) {
         throw new Error('svn path need year end!');
     }
 
 }
 //默认格式化 svn地址不已/结尾
-//argv.svn = argv.svn.replace(/\/$/, '');
+//devConfig.svnPath = devConfig.svnPath.replace(/\/$/, '');
 
-let svnConfig = argv.svn ? {
+let svnConfig = devConfig.svnPath ? {
     //svn trunk 地址
-    svn: argv.svn + '/' + projectName,
+    svn: devConfig.svnPath + '/' + projectName,
     //svn qb 输入发布代码路径
-    onlinePath: argv.svn + '/' + projectName + '/assets/',
+    onlinePath: devConfig.svnPath + '/' + projectName + '/assets/',
     //svn qb 输入标签路径
-    tagPath: argv.svn.replace('trunk', 'tags') + '/' + projectName + '/',
+    tagPath: devConfig.svnPath.replace('trunk', 'tags') + '/' + projectName + '/',
     //标签名字
     tagName: projectName,
     //标签内新增目录
     addPath: 'news/items/' + svnYear + '/' + projectName + '/'
 } : null;
 
+// console.log(svnConfig);
 
-if (argv.svn) {
+if (devConfig.svnPath) {
     svnHandler.init(argv.dir, svnConfig, function(projectPath) {
         dirHandler.synBuildSubDir(projectPath);
         fileHandler.copy({
-            devPubilcPath: argv.devPubilcPath,
-            onLinePubilcPath: argv.onLinePublicPath
+            devPubilcPath: devConfig.devHost,
+            onLinePubilcPath: devConfig.onLinePath
         }, svnConfig, projectPath, function() {
             console.log('project build done!');
             console.log('project dir:%s', projectPath);
@@ -70,8 +76,8 @@ if (argv.svn) {
     dirHandler.initRootDir(argv.dir, function(projectPath) {
         //absPath 是项目生成的根目录路径
         fileHandler.copy({
-            devPubilcPath: argv.devPubilcPath,
-            onLinePubilcPath: argv.onLinePublicPath
+            devPubilcPath: devConfig.devHost,
+            onLinePubilcPath: devConfig.onLinePath
         }, svnConfig, projectPath, function() {
             console.log('project build done!');
             console.log('project dir:%s', projectPath);
